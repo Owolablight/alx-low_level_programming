c9915a17d2f6dc3f4dade5a2426ec0ad0c9e7de9
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-08 15:41:26 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 	char acharacter;
 	int ainteger;
 	long along;
-	long long alonglong;
+	long long my_very_long;
 	float afloat;
 
 	printf("Size of char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of long: %lu byte(s)\n", sizeof(along));
-	printf("Size of long long: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a very very int: %lu byte(s)\n", sizeof(my_very_long));
 	printf("Size of a float: %lu byte(s)\n," sizeof(afloat));
 
 	return (0)
