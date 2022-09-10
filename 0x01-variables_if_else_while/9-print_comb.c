d8910f51ff21f7eb2904fa05f81a11f8ee8e1650
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-09 21:33:27 -0700

    if and else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,21 +7,18 @@
 
 int main (void)
 {
-	char a;
+	int a;
 
 	for (a = 0; a <= 9; a++)
 	{
 		putchar(a + '0');
-	}
-	if (a < 9)
+		if (a < 9)
 	{
 		putchar(',');
-	}
-	{
 		putchar(' ');
 	}
-	{
-		putchar('\n');
 	}
+		putchar('\n');
+	
 	return (0);
 }
