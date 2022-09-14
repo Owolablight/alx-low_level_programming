49f23aad69b62a3a7fcdffee9e089878a0e24ce6
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-14 04:14:10 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - print abs value
+ * @c: int argument of function
+ * Return: 0
+ */
+
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+
+	{
+		return (c);
+	}
+	else
+
+		return(c * -1);
+}
