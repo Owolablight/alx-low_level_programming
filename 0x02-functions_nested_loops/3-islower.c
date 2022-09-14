46285686671356bd61f439714013d121bc3cd807
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-14 00:24:14 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >='a'&& c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
