4e6261c5700207e86631b5462e216d550ba1eadd
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-14 10:47:19 -0700

    two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add integers
+ * @num1: first integer
+ * @num2: second integer
+ * Return: Always 0.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
