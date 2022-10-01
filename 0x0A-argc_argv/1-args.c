583b23a3f7f26bf2dbd7bbc1ad263e55b6053a24
Owolablight <owolabi.oluwadare460@gmail.com>
2022-09-30 18:56:39 -0700

    artc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the number of argument
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
