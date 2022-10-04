9e61f74f50ee519d1e3307259482f0dc806d708c
Owolablight <owolabi.oluwadare460@gmail.com>
2022-10-04 09:21:34 -0700

    memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 /**
  * *str_concat - concatenates two strings
@@ -9,30 +9,40 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
+
 	int i, j;
+
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	i = j = 0;
+
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
 		j++;
 	strDup = malloc(sizeof(char) * (i + j + 1));
+
 	if (strDup == NULL)
 		return (NULL);
+
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		strDup[i] = s1[i];
+
 		i++;
 	}
 	while (s2[j] != '\0')
 	{
 		strDup[i] = s2[j];
+
 		i++, j++;
 	}
 	strDup[i] = '\0';
+
 	return (strDup);
 }
